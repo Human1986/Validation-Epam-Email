e760e690b5e49e5e243a7282f17266b2e08718ea
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-08-03 13:25:03 +0200

    OK

--- src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
@@ -1,13 +1,19 @@
 package com.epam.rd.autotasks.validations;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EpamEmailValidation {
 
     public static boolean validateEpamEmail(String email) {
-        // Put your code here
+        String domain = "@epam.com";
+        if (email == null || ! email.endsWith(domain)) return false;
 
+        Matcher matcher = Pattern.compile("([a-z]_[a-z])").matcher(email);
+
+        return matcher.find();
     }
 }
 
